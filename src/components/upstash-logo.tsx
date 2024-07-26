b76781f74dc3ed96dc1031921f719a1dcdc8ebc6
Despierta852hz <131037133+Despierta852hz@users.noreply.github.com>
2024-07-25 19:18:48 -0600

    Update upstash-logo.tsx

--- src/components/upstash-logo.tsx
+++ src/components/upstash-logo.tsx
@@ -1,11 +1,11 @@
 import React, { HTMLProps } from "react";
 
-export interface Props extends HTMLProps<SVGSVGElement> {
-  size?: number;
+export interface Props extends HTMLProps<HTMLImageElement> {
+  height?: number;
 }
 
 export default function UpstashLogo({ height = 20, ...props }: Props) {
   return (
-    <img src="/despierta.png" height={height} {...props} />
+    <img src="/despierta.png" height={height} alt="Upstash Logo" {...props} />
   );
 }
