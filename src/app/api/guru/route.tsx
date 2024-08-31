7bf1e6d21bc97bbb0039658a79dd6d62cfa0f4ef
nassimmninou <130867963+nassimmninou@users.noreply.github.com>
2024-08-31 19:12:54 +0100

    Update route.tsx

--- src/app/api/guru/route.tsx
+++ src/app/api/guru/route.tsx
@@ -104,8 +104,8 @@ export async function POST(req: NextRequest) {
     );
 
     const tool = createRetrieverTool(retriever, {
-      name: "Cannabis information",
-      description: "Use it to fetch information and specific recommandation from cbdmex about medicinal cannabis ",
+      name: "Despierta-General-Knowledge",
+      description: "Use it to fetch information and specific recommandation from despierta  ",
     });
 
     const default_prompt = `
