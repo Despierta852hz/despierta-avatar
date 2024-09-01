dcf1d1739193fb986cb24363e66635b14ea0e317
Despierta852hz <131037133+Despierta852hz@users.noreply.github.com>
2024-09-01 13:32:05 -0600

    Update route.tsx usando gpt 4o

--- src/app/api/guru/route.tsx
+++ src/app/api/guru/route.tsx
@@ -85,7 +85,7 @@ export async function POST(req: NextRequest) {
     const currentMessageContent = messages[messages.length - 1].content;
 
     const chatModel = new ChatOpenAI({
-      modelName: "gpt-4o-mini",
+      modelName: "gpt-4o",
       temperature: 0.2,
       streaming: true,
     });
